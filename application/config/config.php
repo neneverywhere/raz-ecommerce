08e4f43591c9ecb7859215c98d36831146a823c1
Nen Everywhere <neneverywhere@gmail.com>
2017-11-30 14:27:35 +0700

    k

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://neneveywhere.herokuapp.com/';
+$config['base_url'] = 'http://neneveywhere.herokuapp.com/';
 $config['frontend_url']= $config['base_url'].'/public/frontend/';
 
 /*
